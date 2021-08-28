a8bb8169facc49963afb6ca465e851fff3ef8786
Richard Harris <richardharris916@gmail.com>
2021-08-28 16:34:19 -0500

    Updated Collection methods to return Documents Added Document class

--- src/FCollection.js
+++ src/FCollection.js
@@ -1,3 +1,5 @@
+const Document = require("./FDocument");
+
 class Collection {
   constructor(name, client, query) {
     this.name = name;
@@ -7,36 +9,57 @@ class Collection {
 
   // CREATE
 
-  create(data) {
-    return this.client.query(
+  async create(data) {
+    let documents = await this.client.query(
       this.query.Create(this.query.Collection(this.name), { data })
     );
+
+    return documents.data.map((doc) => new Document(doc));
   }
 
   // READ
 
-  getById(id) {
-    return this.client.query(
+  async getAll() {
+    let documents = await this.client.query(
+      this.query.Map(
+        this.query.Paginate(
+          this.query.Documents(this.query.Collection(this.name))
+        ),
+        this.query.Lambda((x) => this.query.Get(x))
+      )
+    );
+
+    return documents.data.map((doc) => new Document(doc));
+  }
+
+  async getById(id) {
+    let documents = await this.client.query(
       this.query.Get(this.query.Ref(this.query.Collection(this.name), id))
     );
+
+    return documents.data.map((doc) => new Document(doc));
   }
 
   // UPDATE
 
-  update(id, data) {
-    return this.client.query(
+  async update(id, data) {
+    let documents = await this.client.query(
       this.query.Update(this.query.Ref(this.query.Collection(this.name), id), {
         data,
       })
     );
+
+    return documents.data.map((doc) => new Document(doc));
   }
 
   // DELETE
 
-  deleteById(id) {
-    return this.client.query(
+  async deleteById(id) {
+    let documents = await this.client.query(
       this.query.Delete(this.query.Ref(this.query.Collection(this.name), id))
     );
+
+    return documents.data.map((doc) => new Document(doc));
   }
 }
 
---
+++ src/FDocument.js
@@ -0,0 +1,19 @@
+const Ref = require("./FRef");
+
+class Document {
+  constructor(document) {
+    // this = {...document.data}
+    this.id = document.ref.id;
+    this.ref = new Ref(document.ref);
+    this.timestamp = document.ts;
+
+    let keys = Object.keys(document.data);
+    for (let x = 0; x < keys.length; x++) {
+      let key = keys[x];
+      let value = document.data[key];
+      this[key] = value;
+    }
+  }
+}
+
+module.exports = Document;
