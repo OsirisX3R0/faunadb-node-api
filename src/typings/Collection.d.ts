118294442a6e626e51a832b9a46492b79b9eb40e
Richard Harris <richardharris916@gmail.com>
2021-09-18 18:30:51 -0500

    Working on type definitions

---
+++ src/typings/Collection.d.ts
@@ -0,0 +1,50 @@
+import faunadb from 'faunadb'
+import Document from './Document'
+
+/** Represents a collection in the FaunaDB database */
+declare class Collection {
+  /**
+   * Creates a new Collection instance
+   * @param name Name of the collection
+   * @param client `Client` for querying the database
+   * @param query Querying utilities
+   */
+  constructor(name: string, client: faunadb.Client, query: object)
+
+  /** Name of the collection */
+  name: string
+  /** `Client` for querying the database */
+  client: faunadb.Client
+  /** Querying utilities */
+  query: object
+
+  /**
+   * Creates one document in the collection
+   * @param data The data for the document
+   */
+  create(data: object): Promise<void>
+  
+  /** Returns all documents from the collection */
+  getAll(): Promise<Document[]>
+
+  /**
+   * Returns a single document from the collection
+   * @param id Id to query for
+   */
+  getById(id: string): Promise<Document>
+
+  /**
+   * Updates one document in the collection, by Id
+   * @param id Id of the document to update
+   * @param data Updated data for the document
+   */
+  update(id: string, data: object): Promise<void>
+
+  /**
+   * Deletes one document in the collection, by Id
+   * @param id Id of the document to delete
+   */
+  deleteById(id: string): Promise<void>
+}
+
+export default Collection
---
+++ src/typings/Index.d.ts
@@ -0,0 +1,27 @@
+import faunadb from "faunadb";
+
+import Ref from "./Ref";
+
+type Values = (string|number|boolean)[]
+
+/** Represents an Index used to query collections by particular parameters */
+declare class Index {
+  /**
+   * Creates a new Index instance
+   * @param name The name of the index
+   * @param client The FaunaDB `Client` used to query the database
+   * @param query Query utilities
+   */
+  constructor(name: string, client: faunadb.Client, query: object)
+
+  /** Name of the index */
+  name: string
+  /** `Client` for querying the database */
+  client: faunadb.Client
+  /** Querying utilities */
+  query: object
+
+  get(values: Values): Promise<Ref[]>
+}
+
+export default Index
